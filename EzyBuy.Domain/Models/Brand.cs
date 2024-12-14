c69a6253c33185db5ded638566697b414b54a9cf
Quan Nguyen <quan.nguyen.se@gmail.com>
2024-12-14 21:12:46 +0700

    Add project files.

---
+++ EzyBuy.Domain/Models/Brand.cs
@@ -0,0 +1,7 @@
+﻿namespace EzyBuy.Domain.Models;
+
+public class Brand
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = default!;
+}
---
+++ EzyBuy.Domain/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EzyBuy.Domain.Models;
+
+public class Product
+{
+	public int Id { get; set; }
+
+	[Required]
+	public string Name { get; set; } = default!;
+
+	public string Description { get; set; } = default!;
+
+	public decimal Price { get; set; }
+
+	public string PictureFile { get; set; } = default!;
+
+	public int CategoryId { get; set; }
+
+	public Category Category { get; set; } = default!;
+
+	public int BrandId { get; set; }
+
+	public Brand Brand { get; set; } = default!;
+
+}
